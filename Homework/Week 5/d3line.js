338474ac9236a59167c7818b27d9ba2734b68c6d
LunarLite <mdtozer@hotmail.com>
2017-12-01 15:05:20 +0100

    Semi-working cursor on line

--- Homework/Week 5/d3line.js
+++ Homework/Week 5/d3line.js
@@ -54,7 +54,7 @@ function useData(dataName)
 	{
 	  if (error) throw error;		  
 		  
-
+		var color = d3.scale.category10();
 		// Store data, starting from 3rd (0-1-2) column, thus storing only the wind.
 		var winds = data.columns.slice(2).map(function(id) 
 		{
@@ -107,7 +107,6 @@ function useData(dataName)
 			.style("stroke", function(d) { return z(d.id); })
 			
 		// Add text at the end
-		
 		wind.append("text")
 			.datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
 			.attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.windsnelheidgem) + ")"; })
@@ -115,12 +114,99 @@ function useData(dataName)
 			.attr("dy", "0.35em")
 			.style("font", "10px sans-serif")
 			.text(function(d) { return d.id; });
-			 
-		/*
-		 .on('mouseover', function(d){
-				console.log(d);
+
+		// Create mouse object
+		var mouseG = svg.append("g")
+			.attr("class", "mouse-over-effects");
+			
+		mouseG.append("path") // this is the black vertical line to follow mouse
+			.attr("class", "mouse-line")
+			.style("stroke", "black")
+			.style("stroke-width", "1px")
+			.style("opacity", "0");
+			
+		var lines = document.getElementsByClassName('line');
+
+		var mousePerLine = mouseG.selectAll('.mouse-per-line')
+			.data(winds)
+			.enter()
+				.append("g")
+				.attr("class", "mouse-per-line");
+			
+		mousePerLine.append("circle")
+			.attr("r", 7)
+			.style("stroke", function(d) {
+				return color(d.name);
 			})
-		*/
+			.style("fill", "none")
+			.style("stroke-width", "1px")
+			.style("opacity", "0");
+
+		mousePerLine.append("text")
+			.attr("transform", "translate(10,3)");
+	  
+		mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
+			.attr('width', width) // can't catch mouse events on a g element
+			.attr('height', height)
+			.attr('fill', 'none')
+			.attr('pointer-events', 'all')
+			.on('mouseout', function() 
+			{ // on mouse out hide line, circles and text
+				d3.select(".mouse-line")
+					.style("opacity", "0");
+				d3.selectAll(".mouse-per-line circle")
+					.style("opacity", "0");
+				d3.selectAll(".mouse-per-line text")
+					.style("opacity", "0");
+			})
+			.on('mouseover', function() 
+			{ // on mouse in show line, circles and text
+				d3.select(".mouse-line")
+					.style("opacity", "1");
+				d3.selectAll(".mouse-per-line circle")
+					.style("opacity", "1");
+				d3.selectAll(".mouse-per-line text")
+					.style("opacity", "1");
+			})
+			.on('mousemove', function() 
+			{ // mouse moving over canvas
+				var mouse = d3.mouse(this);
+				d3.select(".mouse-line")
+					.attr("d", function() 
+					{
+						var d = "M" + mouse[0] + "," + height;
+						d += " " + mouse[0] + "," + 0;
+						return d;
+					});
+					d3.selectAll(".mouse-per-line")
+				.attr("transform", function(d, i) 
+				{
+					var xDate = x.invert(mouse[0]),
+					bisect = d3.bisector(function(d) 
+					{ return d.date; }).right;
+					idx = bisect(d.values, xDate);
+            
+					var beginning = 0,
+					end = lines[i].getTotalLength(),
+					target = null;
+
+					while (true){
+					  target = Math.floor((beginning + end) / 2);
+					  pos = lines[i].getPointAtLength(target);
+					  if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+						  break;
+					  }
+					  if (pos.x > mouse[0])      end = target;
+					  else if (pos.x < mouse[0]) beginning = target;
+					  else break; //position found
+					}
+            
+					d3.select(this).select('text')
+					  .text(y.invert(pos.y).toFixed(2));
+				  
+					return "translate(" + mouse[0] + "," + pos.y +")";
+				});
+      });
 	});
 
 	function type(d, _, columns) 
